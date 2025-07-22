cce41284d10989e10c4926c46c1052b3c910b103
Joe Smallwood <joe.smallwood@kausal.tech>
2025-07-22 15:00:34 +0300

    Update toggle bar styles to match cards

--- src/components/SettingsToggleBar.tsx
+++ src/components/SettingsToggleBar.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 import Container from '@mui/material/Container';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Stack from '@mui/material/Stack';
@@ -18,15 +20,19 @@ type Props = {
 
 export function SettingsToggleBar({ title, label, value, onChange, isLoading = false }: Props) {
   return (
-    <Container disableGutters sx={{ backgroundColor: 'background.paper', px: 2, py: 0.5 }}>
-      <Stack direction="row" alignItems="center" spacing={1}>
-        <Typography variant="h5">{title}</Typography>
-        <FormControlLabel
-          control={<Switch checked={value} onChange={() => onChange(!value)} />}
-          label={label}
-        />
-        <DelayedCircularProgress isLoading={isLoading} size={16} />
-      </Stack>
+    <Container disableGutters>
+      <Card>
+        <CardContent sx={{ px: 2, py: 0.5, '&:last-child': { pb: 0.5 } }}>
+          <Stack direction="row" alignItems="center" spacing={1}>
+            <Typography variant="h5">{title}</Typography>
+            <FormControlLabel
+              control={<Switch checked={value} onChange={() => onChange(!value)} />}
+              label={label}
+            />
+            <DelayedCircularProgress isLoading={isLoading} size={16} />
+          </Stack>
+        </CardContent>
+      </Card>
     </Container>
   );
 }
