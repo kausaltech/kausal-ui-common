333ae338551f9992e7c994fd7e91e13efac32b88
Joe Smallwood <joe.smallwood@kausal.tech>
2025-08-11 12:06:18 +0300

    Migrate base chart component to common

---
+++ src/components/Chart.tsx
@@ -0,0 +1,165 @@
+import { useEffect, useRef } from 'react';
+
+import { BarChart, CustomChart, LineChart, PieChart } from 'echarts/charts';
+import {
+  DatasetComponent,
+  GraphicComponent,
+  GridComponent,
+  LegendComponent,
+  MarkAreaComponent,
+  MarkLineComponent,
+  TitleComponent,
+  TooltipComponent,
+  TransformComponent,
+} from 'echarts/components';
+import * as echarts from 'echarts/core';
+import { LabelLayout, UniversalTransition } from 'echarts/features';
+import { CanvasRenderer } from 'echarts/renderers';
+import throttle from 'lodash/throttle';
+
+import { useBaseTheme } from '@common/providers/CommonThemeProvider';
+
+import { getChartTheme } from './chart-theme';
+
+echarts.use([
+  BarChart,
+  CustomChart,
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  LegendComponent,
+  LabelLayout,
+  UniversalTransition,
+  CanvasRenderer,
+  GraphicComponent,
+  LineChart,
+  MarkLineComponent,
+  MarkAreaComponent,
+  PieChart,
+]);
+
+// Hack to add margin on the chart to fit the legend
+// Based on https://github.com/apache/echarts/issues/15654
+// Assumes that the legend is at the bottom of the chart
+const resizeLegend = (chart: echarts.ECharts) => {
+  if (chart) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
+    const found = (chart as any)._componentsViews.find(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+      (entry: any) => entry.type === 'legend.plain'
+    );
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const myLegendHeight: number = found?._backgroundEl?.shape?.height || 0;
+    chart.setOption({
+      grid: { bottom: myLegendHeight + 48 },
+    });
+  }
+};
+
+type Props = {
+  isLoading: boolean;
+  data?: echarts.EChartsCoreOption;
+  height?: string;
+  onZrClick?: (clickedDataIndex: [number, number]) => void;
+  className?: string;
+  // Resize the legend when the chart loaded or resized, also adds additional space to the bottom of the chart
+  withResizeLegend?: boolean;
+};
+
+export function Chart({
+  isLoading,
+  data,
+  height = '400px',
+  onZrClick,
+  className,
+  withResizeLegend = true,
+}: Props) {
+  const chartRef = useRef<echarts.ECharts | null>(null);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const theme = useBaseTheme();
+
+  // Initialize the chart
+  useEffect(() => {
+    const chart = echarts.init(wrapperRef.current, getChartTheme(theme).theme);
+
+    chartRef.current = chart;
+
+    const throttledResize = throttle(
+      () => {
+        chart.resize();
+
+        if (withResizeLegend) {
+          resizeLegend(chart);
+        }
+      },
+      1000,
+      {
+        leading: false,
+        trailing: true,
+      }
+    );
+
+    window.addEventListener('resize', throttledResize);
+
+    return () => {
+      throttledResize.flush();
+      window.removeEventListener('resize', throttledResize);
+      chart.clear();
+      chart.dispose();
+    };
+  }, [theme, withResizeLegend]);
+
+  // Show/hide the loading indicator
+  useEffect(() => {
+    if (chartRef.current) {
+      if (isLoading) {
+        chartRef.current.showLoading();
+      } else {
+        chartRef.current.hideLoading();
+      }
+    }
+  }, [isLoading]);
+
+  // Update the chart when the data changes
+  useEffect(() => {
+    if (chartRef.current && data) {
+      chartRef.current.setOption(data);
+
+      if (withResizeLegend) {
+        resizeLegend(chartRef.current);
+      }
+    }
+  }, [data, withResizeLegend]);
+
+  // Add click handler to the chart
+  useEffect(() => {
+    const chart = chartRef.current;
+    const chartZr = chart?.getZr();
+    const withClickHandler = typeof onZrClick === 'function';
+
+    function handleClick(params: { offsetX: number; offsetY: number }) {
+      if (chart && withClickHandler) {
+        const pointInPixel = [params.offsetX, params.offsetY];
+        const pointInGrid = chart.convertFromPixel('grid', pointInPixel);
+
+        // Ensure we have a valid coordinate pair
+        if (Array.isArray(pointInGrid) && pointInGrid.length >= 2) {
+          const dataIndex: [number, number] = [pointInGrid[0], pointInGrid[1]];
+          onZrClick(dataIndex);
+        }
+      }
+    }
+
+    if (chartZr && typeof onZrClick === 'function') {
+      chartZr.on('click', handleClick);
+
+      return () => {
+        chartZr.off('click', handleClick);
+      };
+    }
+  }, [onZrClick]);
+
+  return <div ref={wrapperRef} className={className} style={{ height }} />;
+}
